6058550411db17c16d73597aab831ad2604d3097
Leandro Faria <leandroffaria@gmail.com>
2020-05-13 22:51:53 -0300

    .

--- src/pages/Product/index.jsx
+++ src/pages/Product/index.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import axios from 'axios';
 
 class Product extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            id: props.match.params.id,
+            data: {
+                id: null
+            },
+        }
+    }
+
+    componentDidMount() {
+        axios.get(`https://api.mercadolibre.com/items/${this.state.id}`)
+            .then(({ data }) => {
+                this.setState({ data });
+            })
+    }
+
     render() {
+        console.log('data', this.state.data);
         return (<div>
-            Página de produto
         </div>);
     }
 }
