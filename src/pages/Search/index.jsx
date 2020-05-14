bc0cb7d219b1d12781cb58887d22fb88080e3cb9
Leandro Faria <leandroffaria@gmail.com>
2020-05-13 22:00:25 -0300

    search

--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -1,8 +1,45 @@
 import React from 'react';
+import axios from 'axios';
 
 class Search extends React.Component {
+    state = {
+        results: [],
+    }
+
+    onSearch = (event) => {
+        const { value } = event.currentTarget;
+
+        return axios.get(`https://api.mercadolibre.com/sites/MLB/search?q=${value}`)
+            .then(({ data }) => {
+                const { results } = data;
+
+                this.setState({ results });
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+    }
+
+    renderItems() {
+        const { results } = this.state;
+        
+        return results.map(({ id, title, price }) => (
+            <div key={ `key-${id}` }>
+                <span>{ id }</span>
+                <span>{ title }</span>
+                <span>{ price }</span>
+            </div>
+        ));
+    }
+
     render() {
-        return (<div>Oi sou a pagina de Search</div>);
+        return (<div>
+            <input type="text" onChange={ this.onSearch } />
+            
+            <div>
+                { this.renderItems() }
+            </div>
+        </div>);
     }
 }
 
